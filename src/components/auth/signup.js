47f6ef68c85c9faf8cfa92e540ef13ed58c67cd9
EmiYoda <emilianodelvalle@icloud.com>
2020-08-12 22:18:16 -0500

    built sign in and sign up routes and components

---
+++ src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+    render() {
+        return (
+            <div className='sign-up'>
+                sign up
+            </div>
+        )
+    }
+}
+
+export default SignUp
