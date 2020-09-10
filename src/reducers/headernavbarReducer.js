1bc73e1b522280964efba45711e69f20c8084ece
EmiYoda <emilianodelvalle@icloud.com>
2020-09-09 20:18:14 -0500

    connected header and navbar components to redux store to get links

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -1,25 +1,38 @@
-import {
-    SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
-} from '../actions/types';
+import { SET_HEADER_LINKS, SET_NAVBAR_LINKS } from "../actions/types";
 
 const INITIAL_STATE = {
-    headerLinks: [],
-    navbarLinks: []
-}
+  headerLinks: [
+    {
+      title: "yooo",
+    },
+    {
+      title: "YOOO",
+    },
+  ],
 
-export default function(state = INITIAL_STATE, action) {
-    switch (action.type) {
-        case SET_HEADER_LINKS:
-            return {
-                ...state,
-                headerLinks: action.payload
-            }
-        case SET_NAVBAR_LINKS:
-            return {
-                ...state,
-                navbarLinks: action.payload
-            }
-        default: return state;
-    }
+  navbarLinks: [
+    {
+      title: "account",
+    },
+    {
+      title: "purchases",
+    },
+  ],
+};
+
+export default function (state = INITIAL_STATE, action) {                                     
+  switch (action.type) {
+    case SET_HEADER_LINKS:
+      return {
+        ...state,
+        headerLinks: action.payload,
+      };
+    case SET_NAVBAR_LINKS:
+      return {
+        ...state,
+        navbarLinks: action.payload,
+      };
+    default:
+      return state;
+  }
 }
