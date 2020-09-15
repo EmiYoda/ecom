0c37323397023472210f17ee069df634b9a4c7a4
EmiYoda <emilianodelvalle@icloud.com>
2020-09-15 14:08:22 -0500

    built base structure for account components

---
+++ src/components/account/purchaseHistory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PurchaseHistory extends Component {
+    render() {
+        return(
+            <div>
+                Purchase history
+            </div>
+        )
+    } 
+}
+
+export default PurchaseHistory;
--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -1,41 +1,23 @@
-import {
-    SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
-} from '../actions/types';
+import { SET_HEADER_LINKS, SET_NAVBAR_LINKS } from "../actions/types";
 
 const INITIAL_STATE = {
-    headerLinks: [
-        {
-            title: 'yooo'
-        },
-        {
-            title: 'YOOO'
-        }
-    ],
-    navbarLinks: [
-        {
-            title: 'account',
-            active: true
-        },
-        {
-            title: 'purchases',
-            active: false
-        }
-    ]
-}
+  headerLinks: [],
+  navbarLinks: []
+};
 
-export default function(state = INITIAL_STATE, action) {
-    switch (action.type) {
-        case SET_HEADER_LINKS:
-            return {
-                ...state,
-                headerLinks: action.payload
-            }
-        case SET_NAVBAR_LINKS:
-            return {
-                ...state,
-                navbarLinks: action.payload
-            }
-        default: return state;
-    }
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case SET_HEADER_LINKS:
+      return {
+        ...state,
+        headerLinks: action.payload,
+      };
+    case SET_NAVBAR_LINKS:
+      return {
+        ...state,
+        navbarLinks: action.payload,
+      };
+    default:
+      return state;
+  }
 }
