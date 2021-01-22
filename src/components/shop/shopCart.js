3457769c0d9643ac3ec8c745c544dccc1b06cc9e
EmiYoda <emilianodelvalle@icloud.com>
2021-01-22 08:22:32 -0600

    setup order review component and added page title component

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import CartButton from './cartButton';
 
+import history from '../../history';
+
 function CartContent({className, products}) {
     let count = products.length;
     let productsJSX = products.map(product => <CartProduct {...product} key={product._id} />);
@@ -25,7 +27,7 @@ function CartFooter({className, products}) {
     const price = 7.99;
     return (
         <div className={`${className} cart-footer`}>
-            <a className='cart-footer__checkout'>
+            <a onClick={() => history.push('/order/review')} className='cart-footer__checkout'>
                 Checkout
             </a>
             <div className='cart-footer__subtotal'>
