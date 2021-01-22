110128849e45d219830f7bfe56a3730f8144fe4d
EmiYoda <emilianodelvalle@icloud.com>
2021-01-21 22:16:30 -0600

    finished styling cart

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -47,7 +47,7 @@ class Shop extends Component {
 
     render() {
 
-        return <ShopCart className='shop__cart'/>
+        // return <ShopCart className='shop__cart'/>
 
         return (
             <div className='shop'>
