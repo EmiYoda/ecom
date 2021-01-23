f7658af8c0739692635ea4317d50ae8d1d6115ef
EmiYoda <emilianodelvalle@icloud.com>
2021-01-22 22:15:15 -0600

    built the information routes and container payment and shipping js

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -21,7 +21,7 @@ class ReviewForm extends Component {
                 <div className='review-form__line'></div>
 
                 <Field className='review-form__proceed'
-                onClick={() => history.push('/signin')}
+                onClick={() => history.push('/information/shipping')}
                 type='submit'
                 title='Proceed to Checkout'
                 name='proceed'
